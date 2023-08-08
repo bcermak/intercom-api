264f49260e17b3a27c88a9177787b72511959296
Brandon Cermak <cermakprofessional@gmail.com>
2023-08-08 14:36:05 -0500

    added webhook endpoint and first API call

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-// const Intercom = require('intercom-client')
-// const client = new Intercom.Client({  tokenAuth: {token:'dG9rOmQ1MzBhZjAwXzY2MGZfNGI3N19hNmVhXzJmOWM3Yjc1NzZkODoxOjA='} });
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// client.useRequestOpts({
-//     headers: {
-//         'Intercom-Version': 2.9
-//     }
-// });
+const app = express().use(bodyParser.json());
 
-// const user = await client.contacts.createUser({
-//     name: 'Brandon Cermak',
-//     email: 'b.cermak@outlook.com',
-//     custom_attributes: {
-//         isScholar: 'true'
-//     }
-// });
+// receiving webhook notification from Intercom
+app.post('/', (req, res) => {
+    let data = req.body;
+    console.log(data)
+});
 
+
+//reply to conversation via Intercom API
 const sdk = require('api')('@intercom-api-reference/v2.9#v67gwo2lhkg10ra');
 
-sdk.auth('dG9rOmQ1MzBhZjAwXzY2MGZfNGI3N19hNmVhXzJmOWM3Yjc1NzZkODoxOjA=');
-sdk.createContact({
-  email: 'b.cermak@outlook.com',
-  name: 'Brandon Cermak'
+sdk.auth('dG9rOmUyMTA4M2Y4XzU4ZWRfNGNkYl9hN2ZiXzFjNzlkNmVkNGNlZDoxOjA=');
+sdk.server('https://api.intercom.io');
+sdk.replyConversation({
+  message_type: 'comment',
+  type: 'admin',
+  body: 'admin name is',
+  admin_id: '4660381'
+}, {
+  id: '27466974880',
+  'intercom-version': '2.9'
 })
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> console.log(`listening on port ${port}...`))

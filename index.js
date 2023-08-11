05103e391a33abaea53d78c783d6393da00959ac
Brandon Cermak <cermakprofessional@gmail.com>
2023-08-11 14:57:10 -0500

    made API call to snooze for 48 hours after rating request

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.post('/', (req, res) => {
     let authorID = JSON.stringify(req.body.data.item.conversation_rating.teammate.id);
     let conversationID = req.body.data.item.id;
     let rating = req.body.data.item.conversation_rating.rating;
+    let updatedAt = req.body.data.item.updated_at + 172800;
+
+    console.log(updatedAt)
 
     if (rating == 5) {
       const sdk = require('api')('@intercom-api-reference/v2.9#v67gwo2lhkg10ra');
@@ -23,8 +26,19 @@ app.post('/', (req, res) => {
         id: conversationID,
         'intercom-version': '2.9'
       })
-        .then(({ data }) => console.log(data))
-        .catch(err => console.error(err));
+        .then(()=> {
+
+          sdk.auth('dG9rOmUyMTA4M2Y4XzU4ZWRfNGNkYl9hN2ZiXzFjNzlkNmVkNGNlZDoxOjA=');
+          sdk.server('https://api.intercom.io');
+          sdk.manageConversation({
+            message_type: 'snoozed',
+            type: 'admin',
+            admin_id: '4660381',
+            snoozed_until: updatedAt
+          }, {id: conversationID, 'intercom-version': '2.9'})
+            .then(({ data }) => console.log(data))
+            .catch(err => console.error(err));
+                  })
     } else {
       console.log("Rating is not good")
     }

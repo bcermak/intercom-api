c23cce59ae11badfecf2c2e01385a5c8fd4da251
Brandon Cermak <cermakprofessional@gmail.com>
2023-08-11 15:08:10 -0500

    added admin data via object to use within API functions

--- index.js
+++ index.js
@@ -2,6 +2,298 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express().use(bodyParser.json());
 
+// data for all admins to use within API calls
+const teamAdmins = {
+  "type": "admin.list",
+  "admins": [
+    {
+      "type": "admin",
+      "email": "bill@smile.io",
+      "id": "46571",
+      "name": "Bill Curtis",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "mike@smile.io",
+      "id": "46962",
+      "name": "Mike Rossi",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "katrina@smile.io",
+      "id": "1246186",
+      "name": "Katrina",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "maggie@smile.io",
+      "id": "1314872",
+      "name": "Maggie",
+      "away_mode_enabled": true,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "operator+a6zuo3jn@intercom.io",
+      "id": "3963564",
+      "name": "Smile Assistant",
+      "away_mode_enabled": false,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "tess@smile.io",
+      "id": "3982666",
+      "name": "Tess",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "lynn@smile.io",
+      "id": "4121991",
+      "name": "Lynn Penaloza",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "sarahf@smile.io",
+      "id": "4122070",
+      "name": "Sarah Facini",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "infrastructure@smile.io",
+      "id": "4148511",
+      "name": "Smile SRE Team",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "systems@smile.io",
+      "id": "4172216",
+      "name": "Support Team",
+      "away_mode_enabled": false,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "tim@smile.io",
+      "id": "4235432",
+      "name": "Tim Peckover",
+      "away_mode_enabled": true,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "joana@smile.io",
+      "id": "4334203",
+      "name": "Joana Jacome",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "mack@smile.io",
+      "id": "4583295",
+      "name": "Mackenzie",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "ezra@smile.io",
+      "id": "4656470",
+      "name": "Ezra Alamsyah",
+      "away_mode_enabled": false,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "brandon@smile.io",
+      "id": "4660381",
+      "name": "Brandon Cermak",
+      "away_mode_enabled": true,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "jade@smile.io",
+      "id": "5018703",
+      "name": "Jade Davis",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "karen@smile.io",
+      "id": "5216745",
+      "name": "Karen Fuser",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "david.wyatt@smile.io",
+      "id": "5222292",
+      "name": "David Wyatt",
+      "away_mode_enabled": true,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "mitchell@smile.io",
+      "id": "5391852",
+      "name": "Mitchell Curry",
+      "away_mode_enabled": false,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "andreandanafitriah@smile.io",
+      "id": "5427534",
+      "name": "Andreandana Fitriah",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "nicole.miles@smile.io",
+      "id": "5430357",
+      "name": "Nicole Miles",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "hannah.scrafton@smile.io",
+      "id": "5542987",
+      "name": "Hannah Scrafton",
+      "away_mode_enabled": true,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "lisa.gallagher@smile.io",
+      "id": "5553917",
+      "name": "Lisa Gallagher",
+      "away_mode_enabled": true,
+      "away_mode_reassign": true,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "social@smile.io",
+      "id": "5603938",
+      "name": "Smile Marketing Team",
+      "away_mode_enabled": true,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "angela.thompson@smile.io",
+      "id": "6590114",
+      "name": "Angie T",
+      "away_mode_enabled": false,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    },
+    {
+      "type": "admin",
+      "email": "christian.bullock@smile.io",
+      "id": "6679008",
+      "name": "Christian Bullock",
+      "away_mode_enabled": false,
+      "away_mode_reassign": false,
+      "has_inbox_seat": true,
+      "team_ids": [],
+      "team_priority_level": {}
+    }
+  ]
+}
 // receiving webhook notification from Intercom to identify admin (MXie) and rating
 app.post('/', (req, res) => {
 
@@ -9,8 +301,6 @@ app.post('/', (req, res) => {
     let conversationID = req.body.data.item.id;
     let rating = req.body.data.item.conversation_rating.rating;
     let updatedAt = req.body.data.item.updated_at + 172800;
-
-    console.log(updatedAt)
 
     if (rating == 5) {
       const sdk = require('api')('@intercom-api-reference/v2.9#v67gwo2lhkg10ra');

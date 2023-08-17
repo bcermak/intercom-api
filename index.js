cde3c7af4b23f755d3bb8259c369f7586eb9afb6
Brandon Cermak <cermakprofessional@gmail.com>
2023-08-17 13:40:49 -0500

    changing port

--- index.js
+++ index.js
@@ -211,5 +211,5 @@ app.post('/', (req, res) => {
 });
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, ()=> console.log(`listening on port ${port}...`))
